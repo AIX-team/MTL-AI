0aa636a81d41722aacbd54400a2d5ed68f968ee1
박효찬 <qwes5674@gmail.com>
2025-02-03 02:47:59 +0900

    google map 기능 추가

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, APIRouter
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from routers.googleMap import router as google_map_router 
+from routers.place import router as place
+import os
+from dotenv import load_dotenv
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from routers.googleMap import router as google_map_router # 라우터 설정
+
+load_dotenv()
 
 app = FastAPI()
+router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 #라우터 설정
 app.include_router(google_map_router)
+app.include_router(place)
 
 # 정적 파일 서빙
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -21,6 +29,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# api요청
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    api_key = os.getenv("api_key") # google api키
+    return templates.TemplateResponse("index.html",{"request":request, "api_key":api_key})
 
 # 실행 환경 설정
 if __name__ == "__main__":
